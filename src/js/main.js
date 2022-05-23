27a01b5a134c5d1da172a394b11715f73278c4d3
thenewtonium <60141408+thenewtonium@users.noreply.github.com>
2022-05-23 17:55:12 +0100

    Implemented theme switcher
    very basic for now

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,13 @@ var darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
 darkModeQuery.addListener(function (query) {
 	var theme = query.matches ? "dark" : "light";
-	var signal = new CustomEvent("changeTheme", {
-        detail: {
-          theme: theme
-        }
-    });
-	document.documentElement.dispatchEvent(signal);
+	setTheme(theme);
 });
+
+window.setTheme = function (theme) {
+	document.documentElement.setAttribute("data-theme", theme);
+	sendMessage ({ setConfig: {theme: theme} });
+};
 
 // additional script to align the giscus theme to the page theme
 function sendMessage(message) {
@@ -16,11 +16,3 @@ function sendMessage(message) {
   if (!iframe) return;
   iframe.contentWindow.postMessage({ giscus: message }, iframe.src);
 }
-
-document.documentElement.addEventListener("changeTheme", e => {
-	console.log(e);
-	var theme = e.detail.theme;
-	document.documentElement.setAttribute("data-theme", theme);
-	sendMessage ({ setConfig: {theme: theme} });
-});
-
